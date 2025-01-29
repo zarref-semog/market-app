494a485bc8b44053ffbd76eab18c77f03b610a9d
Murilo Gomes Ferraz <murilo.ferraz@flex-am.com.br>
2025-01-29 16:43:01 -0400

    Finish cart page template

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:store_app_1/apis/category_api.dart';
+import 'package:store_app_1/models/category.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -8,14 +10,142 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  var items;
+
+  @override
+  void initState() {
+    super.initState();
+    items = CategoryApi.getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
-        )),
-        body: Center(child: Text("Cart Page")));
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.remove_shopping_cart, size: 30),
+            onPressed: () {},
+          )
+        ],
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text("Market App",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 26)),
+      ),
+      body: _body(context),
+    );
+  }
+
+  Widget _body(context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("My Cart",
+                    style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700])),
+                SizedBox(height: 10),
+                TextField(
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    suffixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text("Results",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700]))
+              ]),
+        ),
+        FutureBuilder<List<Category>>(
+            future: items,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              if (snapshot.hasError) {
+                return Text(
+                  "Failed to load categories. ${snapshot.error}",
+                  textAlign: TextAlign.center,
+                );
+              }
+
+              if (!snapshot.hasData) {
+                return Text(
+                  "No categories available. ${snapshot.error}",
+                  textAlign: TextAlign.center,
+                );
+              }
+
+              List<Category> items = snapshot.data!;
+
+              return Expanded(
+                child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: SizedBox(
+                          child: Image.network(items[index].image,
+                              fit: BoxFit.cover, width: 100, height: 80),
+                        ),
+                        title: Text(
+                          '${items[index].name}',
+                          style: TextStyle(
+                              fontSize: 18, overflow: TextOverflow.ellipsis),
+                          maxLines: 2,
+                        ),
+                        subtitle:
+                            Text("\$99.99", style: TextStyle(fontSize: 16)),
+                        trailing: SizedBox(
+                          width: 100,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                  child: Container(
+                                    child: Icon(Icons.add, color: Colors.white),
+                                    decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        shape: BoxShape.circle),
+                                  ),
+                                  onTap: () {}),
+                              Text("${99}", style: TextStyle(fontSize: 16)),
+                              GestureDetector(
+                                  child: Container(
+                                    child:
+                                        Icon(Icons.remove, color: Colors.white),
+                                    decoration: BoxDecoration(
+                                        color: Colors.red,
+                                        shape: BoxShape.circle),
+                                  ),
+                                  onTap: () {}),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              );
+            })
+      ],
+    );
   }
 }
